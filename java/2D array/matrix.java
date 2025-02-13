3bb5a0e40f3d9d8894771fc05835f51faaef179a
vedant0517 <vedant.kapgate17@gmail.com>
2025-02-14 00:06:47 +0530

    .

--- java/2D array/matrix.java
+++ java/2D array/matrix.java
@@ -72,13 +72,27 @@ public class matrix{
         endRow--;
     }
     System.out.println();
-
-
     }
+
+
+    public static int diagonalSum(int matrix[][]){
+        int sum=0;
+        for(int i=0;i<matrix.length;i++){
+            sum+=matrix[i][i];
+
+            if(i != matrix.length-i-1){
+                sum+=matrix[i][matrix.length-i-1];
+            }
+        }
+        return sum;
+  
+    }
+    
     public static void main(String argz[]){
 
         int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
         printSpiral(matrix);
+        System.out.println("Diagonal sum : "+diagonalSum(matrix));
        
     }
 }
