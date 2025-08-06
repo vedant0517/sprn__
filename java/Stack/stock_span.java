4c7003f39f3c62e18b97c8bd335e1ce2485c25e2
vedant0517 <vedant.kapgate17@gmail.com>
2025-08-07 01:01:18 +0530

    1

--- java/Stack/stock_span.java
+++ java/Stack/stock_span.java
@@ -6,7 +6,7 @@ public class stock_span {
         s.push(0);
         for(int i=1;i<stocks.length;i++){
             int curPrice=stocks[i];
-            while(!s.isEmpty() && curPrice>stocks[s.peek()]){
+            while(!s.isEmpty() && curPrice>=stocks[s.peek()]){
                 s.pop();
             }
             if(s.isEmpty()){
