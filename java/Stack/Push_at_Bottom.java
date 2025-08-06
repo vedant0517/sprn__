d94790ffe79f43d49f8058d203406c9387ea1db8
vedant0517 <vedant.kapgate17@gmail.com>
2025-08-07 00:48:58 +0530

    .

--- java/Stack/Push_at_Bottom.java
+++ java/Stack/Push_at_Bottom.java
@@ -10,15 +10,35 @@ public class Push_at_Bottom {
         pushAtBottom(s, data);
         s.push(top);
     }
-    public static void main(String args[]){
-        Stack<Integer> s=new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+    //reverse Stack
+    public static void reverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top=s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
 
-        pushAtBottom(s,4);
+    public static void printStack(Stack<Integer> s){
         while(!s.isEmpty()){
             System.out.println(s.pop());
         }
+    }
+
+    public static void main(String args[]){
+        Stack<Integer> s=new Stack<>();
+        s.push(3);
+        s.push(2);
+        s.push(1);
+        //1 2 3
+        // pushAtBottom(s, 4);
+        // while(!s.isEmpty()){
+        //     System.out.println(s.pop());
+        // }
+
+        reverseStack(s);
+        printStack(s);
+        //3 2 1
     }
 }
