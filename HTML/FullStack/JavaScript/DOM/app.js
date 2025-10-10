2ad8efb7f13cca757cc5b6b6fba7254b51eef95e
Vedant Kapgate <vedant.kapgate17@gmail.com>
2025-10-10 10:11:43 +0530

    .

---
+++ HTML/FullStack/JavaScript/DOM/app.js
@@ -0,0 +1,25 @@
+let p1=document.createElement('p');
+p1.innerText="Hey I am red";
+document.querySelector('body').append(p1);
+p1.classList.add('red');
+
+
+let h3=document.createElement('h3');
+h3.innerText="Hey I am a blue h3!";
+document.querySelector('body').append(h3);
+h3.classList.add('blue');
+
+let d=document.createElement('div');
+let h1=document.createElement('h1');
+let p2=document.createElement('p');
+
+h1.innerText="I am in a div";
+p2.innerText="Me too!!";
+
+d.append(h1);
+d.append(p2);
+
+d.classList.add("divbox");
+
+
+document.querySelector('body').append(d);
